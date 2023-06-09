94dcd5e08c5eb78aa9cb97cb81aca34d3105184e
Keita Iisaka <iisak8a@gmail.com>
2023-06-09 06:17:37 +0100

    adjust scroll offset and add pb to about

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -24,7 +24,7 @@ window.addEventListener("scroll", function () {
   navLinksEl.forEach((link) => {
     const targetSection = document.querySelector(link.getAttribute("href"));
     if (targetSection) {
-      const sectionTop = targetSection.offsetTop - 200; // Adjust the offset as needed
+      const sectionTop = targetSection.offsetTop - 250; // Adjust the offset as needed
       const sectionHeight = targetSection.offsetHeight;
 
       if (
@@ -49,7 +49,7 @@ window.addEventListener("DOMContentLoaded", function () {
   navLinksEl.forEach((link) => {
     const targetSection = document.querySelector(link.getAttribute("href"));
     if (targetSection) {
-      const sectionTop = targetSection.offsetTop - 200; // Adjust the offset as needed
+      const sectionTop = targetSection.offsetTop - 250; // Adjust the offset as needed
       const sectionHeight = targetSection.offsetHeight;
 
       if (
