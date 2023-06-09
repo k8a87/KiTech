d172a5ebe7ac96f8eba0cb82e474ae373aebb03c
Keita Iisaka <iisak8a@gmail.com>
2023-06-09 04:07:40 +0100

    add scroll spy js

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,7 +1,7 @@
 // Close nav on click
 
 const menuToggle = document.querySelector(".navbar-collapse");
-const navlinksEl = document.querySelectorAll(".nav-link");
+const navLinksEl = document.querySelectorAll(".nav-link");
 const toggler = document.querySelector(".navbar-toggler");
 
 const closeLinks = () => {
@@ -9,6 +9,56 @@ const closeLinks = () => {
   toggler.classList.toggle("collapsed");
 };
 
-navlinksEl.forEach(link => {
+navLinksEl.forEach((link) => {
   link.addEventListener("click", closeLinks);
+});
+
+//Fix scrollspy (helped byCHATGPT)
+// Get all the nav links
+
+// Add event listener to scroll
+window.addEventListener("scroll", function () {
+  const currentScroll = window.scrollY;
+
+  // Iterate over each nav link and check if its corresponding section is in view
+  navLinksEl.forEach((link) => {
+    const targetSection = document.querySelector(link.getAttribute("href"));
+    if (targetSection) {
+      const sectionTop = targetSection.offsetTop - 200; // Adjust the offset as needed
+      const sectionHeight = targetSection.offsetHeight;
+
+      if (
+        currentScroll >= sectionTop &&
+        currentScroll < sectionTop + sectionHeight
+      ) {
+        // Remove 'active' class from all nav links
+        navLinksEl.forEach((link) => link.classList.remove("active"));
+
+        // Add 'active' class to the current nav link
+        link.classList.add("active");
+      }
+    }
+  });
+});
+
+// Manually activate the appropriate navigation item on page load/refresh
+window.addEventListener("DOMContentLoaded", function () {
+  const currentScroll = window.scrollY;
+
+  // Iterate over each nav link and check if its corresponding section is in view
+  navLinksEl.forEach((link) => {
+    const targetSection = document.querySelector(link.getAttribute("href"));
+    if (targetSection) {
+      const sectionTop = targetSection.offsetTop - 200; // Adjust the offset as needed
+      const sectionHeight = targetSection.offsetHeight;
+
+      if (
+        currentScroll >= sectionTop &&
+        currentScroll < sectionTop + sectionHeight
+      ) {
+        // Add 'active' class to the current nav link
+        link.classList.add("active");
+      }
+    }
+  });
 });
